f61bc5d7164a96c6c6574441c9dac92da45ac0db
Nick <nicknyler@gmail.com>
2025-06-14 13:31:07 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Earthquake
@@ -20,8 +20,19 @@ def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
-
-
+@app.route('/earthquakes/<int:id>')
+def earthquake(id):
+    result=Earthquake.query.filter(Earthquake.id==id).first()
+    if result:
+        return jsonify(result.to_dict()), 200
+    else:
+        
+        return jsonify({'message': f'Earthquake {id} not found.'}), 404
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def magnitude(magnitude):
+    result=Earthquake.query.filter(Earthquake.magnitude>=magnitude).all()
+    count=len(result)
+    return jsonify({"count":count,'quakes':[quake.to_dict() for quake in result]}),200
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
